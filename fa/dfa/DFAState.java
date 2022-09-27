767b6322bc5b0b2b351a5f8993dd93473d0b9bfb
Drew Marshall <drewmarshall@u.boisestate.edu>
2022-09-26 19:38:25 -0600

    Starter code

--- fa/dfa/DFAState.java
+++ fa/dfa/DFAState.java
@@ -1,7 +1,20 @@
 package fa.dfa;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
 import fa.State;
 
 public class DFAState extends State{
     
+    private LinkedHashMap<Character, DFAState> transitionMap;
+
+    public DFAState(String name){
+        this.name = name;
+        transitionMap = new LinkedHashMap<>();
+    }
+
+    public void transition(char symb, DFAState toState){
+        transitionMap.put(symb, toState);
+    }
 }
