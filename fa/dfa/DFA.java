4930aed9f93cf7770a6fa471aea9ffdd34a40511
Drew Marshall <drewmarshall@u.boisestate.edu>
2022-09-26 19:37:41 -0600

    Some starter code

--- fa/dfa/DFA.java
+++ fa/dfa/DFA.java
@@ -9,57 +9,91 @@ public class DFA implements DFAInterface{
 
     //Need to instantiate the 5-tuple with variables to hold values
     private LinkedHashSet<DFAState> states; //states = Q = all states
-    private DFAState startState; //startState = q0 = starting state
     private LinkedHashSet<DFAState> finalStates; // finalStates = F = final states (if any)
     private LinkedHashSet<Character> sigma; //sigma = alphabet 
+    private DFAState startState; //startState = q0 = starting state
 
+    public DFA() {
+        states = new LinkedHashSet<>();
+        finalStates = new LinkedHashSet<>();
+        sigma = new LinkedHashSet<>();
+        startState = null;
+    }
+
+    /**
+	 * Adds the initial state to the DFA instance
+	 * @param name is the label of the start state
+	 */
     @Override
     public void addStartState(String name) {
-        // TODO Auto-generated method stub
-        
+        //Need to check if the startState has already
+        //been entered somewhere else
+        //if not we can just initialize it and add it
+        for(DFAState s: states) {
+            String stateName = s.getName();
+            if(stateName.equals(name)){ //Start state is already in states so we just have to update it
+                startState = s;
+            }       
+        }
+
+        if(startState == null) {
+            startState = new DFAState(name);
+            states.add(startState);
+        }
     }
 
     @Override
     public void addState(String name) {
-        // TODO Auto-generated method stub
+        DFAState state = new DFAState(name);
+        states.add(state);
         
     }
 
     @Override
     public void addFinalState(String name) {
-        // TODO Auto-generated method stub
+        DFAState fState = new DFAState(name);
+        finalStates.add(fState);
+        states.add(fState);
         
     }
 
     @Override
     public void addTransition(String fromState, char onSymb, String toState) {
-        // TODO Auto-generated method stub
-        // Need to implement a Map interface most likely a hashMap
+        DFAState stateFrom = new DFAState(fromState);
+        DFAState stateTo = new DFAState(toState);
+        stateFrom.transition(onSymb, stateTo);
+
+        boolean flag = false;
+        for(Character c: sigma) {
+            if(c.equals(onSymb)) {
+                flag = true;
+            }
+        }
+
+        if(!flag) {
+            sigma.add(onSymb);
+        }
         
     }
 
     @Override
     public Set<? extends State> getStates() {
-        // TODO Auto-generated method stub
-        return null;
+        return states;
     }
 
     @Override
     public Set<? extends State> getFinalStates() {
-        // TODO Auto-generated method stub
-        return null;
+        return finalStates;
     }
 
     @Override
     public State getStartState() {
-        // TODO Auto-generated method stub
-        return null;
+        return startState;
     }
 
     @Override
     public Set<Character> getABC() {
-        // TODO Auto-generated method stub
-        return null;
+        return sigma;
     }
 
     @Override
